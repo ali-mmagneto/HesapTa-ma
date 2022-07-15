13ef7dd40d74434acf16226215e7a1053e58a508
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-07-15 21:37:23 +0300

    Update ubot.py

--- ubot.py
+++ ubot.py
@@ -5,7 +5,7 @@ from pyrogram import Client, filters, enums
 from config import APP_ID, API_HASH, STRING_SESSION, MESAJ
 
 
-userbot = Client(name='userbot', api_id=APP_ID, api_hash=API_HASH, session_string=STRING_SESSION, parse_mode=enums.ParseMode.HTML)
+userbot = Client(session_name=STRING_SESSION, api_id=APP_ID, api_hash=API_HASH, parse_mode=enums.ParseMode.HTML)
 userbot.start()
 
 @Client.on_message(filters.text & filters.private)
