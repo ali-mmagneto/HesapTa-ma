1caa3ff609d92c6d58eca21477fd3a185481e95f
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-07-16 10:46:32 +0300

    Update ubot.py

--- ubot.py
+++ ubot.py
@@ -5,17 +5,17 @@ from pyrogram import Client, filters
 from config import APP_ID, API_HASH, STRING_SESSION, MESAJ
 
 
-userbot = Client(
+UserBot = Client(
         name='userbot',
         session_string=STRING_SESSION, 
         api_id=APP_ID,
         api_hash=API_HASH
         )
 
-@Client.on_message(filters.text & filters.private)
+@UserBot.on_message(filters.text & filters.private)
 async def tsm(c: Client, m: "types.Message"):
     await m.reply_text(
         text=MESAJ,
         chat_id=chat_id)
        
-userbot.run()
+UserBot.run()
