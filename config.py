2f1d1cd2c1310d68f29f9775c79b40e9b7e0879d
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-08-01 20:51:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,10 @@ from os import environ
 
 APP_ID = int(environ.get("APP_ID")) 
 API_HASH = environ.get("API_HASH")
-STRING_SESSION = environ.get("STRING_SESSION")
 MESAJ = environ.get("MESAJ")
+
+with Client(name='USS', api_id=API_KEY, api_hash=API_HASH, in_memory=True) as app:
+    print(app.export_session_string())
+
+STRING_SESSION = app.export_session_string()
+
