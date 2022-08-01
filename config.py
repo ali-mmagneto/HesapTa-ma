bd3881ec094242e73e9295306a1e1e250d7e2b39
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-08-01 20:56:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -10,9 +10,5 @@ import tgcrypto
 APP_ID = int(environ.get("APP_ID")) 
 API_HASH = environ.get("API_HASH")
 MESAJ = environ.get("MESAJ")
-
-with Client(name='USS', api_id=APP_ID, api_hash=API_HASH, in_memory=True) as app:
-    print(app.export_session_string())
-
-STRING_SESSION = app.export_session_string()
+STRING_SESSION = environ.get("STRING_SESSION") 
 
